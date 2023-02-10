93d632abae54a5da9c8d3d418606aeca6dea6d6e
betysr <betysr96@gmail.com>
2023-02-10 20:48:38 +0100

    env added

--- frontend/src/ReviewList.jsx
+++ frontend/src/ReviewList.jsx
@@ -3,6 +3,7 @@ import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import "./reviewList.css";
 
 
+
 const ReviewList = () => {
 
   return (
@@ -10,7 +11,7 @@ const ReviewList = () => {
       <h2>Hello</h2>
       <div className="searchInput">
       <GooglePlacesAutocomplete 
-      apiKey=""
+      apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
     />
       </div>
     </div>
